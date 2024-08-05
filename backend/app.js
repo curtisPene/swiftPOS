b6c6231fcc45861d4eb3d42b7ddfd1f98d5851b7
Curtis <curtispene92@gmail.com>
2024-08-05 21:08:03 +1200

    refactor: Clean up is auth middleware function

--- backend/app.js
+++ backend/app.js
@@ -12,16 +12,10 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-// Jest test route
-// app.use("/", (req, res, next) => {
-//   res.status(200).send("Hello Jest");
-// });
-
+app.use("/", isAuth);
 app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
-  console.log(err.message, err.code);
-
   const status = err.status || 500;
   const message = err.message || "Internal Server Error";
   const code = err.code || "SERVER_INTERNAL_ERROR";
--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -4,17 +4,7 @@ const isAuth = async (req, res, next) => {
   // Get access token from cookies
   const accessToken = req.cookies["swf_access"];
 
-  console.log(accessToken);
-
   if (!accessToken || accessToken === "") {
-    const error = new Error("Authentication failed");
-    error.status = 400;
-    error.code = "USER_NOT_AUTHENTICATED";
-    return next(error);
-  }
-
-  // If no access token throw error with 400
-  if (!accessToken) {
     const error = new Error("Authentication failed");
     error.status = 400;
     error.code = "USER_NOT_AUTHENTICATED";
@@ -25,7 +15,6 @@ const isAuth = async (req, res, next) => {
   try {
     jwt.verify(accessToken, process.env.JWT_SECRET);
   } catch (e) {
-    console.log(chalk.bgGreenBright(e.name));
     if (e.name === "TokenExpiredError") {
       const error = new Error("Authentication failed");
       error.status = 401;
@@ -39,7 +28,8 @@ const isAuth = async (req, res, next) => {
       error.code = "USER_MALFORMED_ACCESS_TOKEN";
       return next(error);
     }
-    return next(error);
+
+    return next(e);
   }
 
   next();
