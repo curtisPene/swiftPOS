860ddd5181a7d553e65496032aa8ae7c02fb8180
Curtis Pene <curtispene92@gmail.com>
2024-08-07 12:06:54 +1200

    refactor: work on sedding script

--- backend/tests/adminTests/seedDatabase.js
+++ backend/tests/adminTests/seedDatabase.js
@@ -28,64 +28,7 @@ const generateAccessToken = (userId, locationId, role) => {
 const seedDatabase = async () => {
   try {
     await connectDB();
-
-    // Clear existing data
-    await User.deleteMany({});
-    await Location.deleteMany({});
-    await Product.deleteMany({});
-    await ProductVariant.deleteMany({});
-
-    // Create a test user
-    const user = await User.create({
-      email: "user@gmail.com",
-      password: "password",
-      firstName: "curtis",
-      lastName: "pene",
-      role: "admin",
-    });
-
-    // Create a test location associated with the user
-    const location = await Location.create({
-      name: "Location1",
-      admin: user._id,
-      users: [user._id],
-    });
-
-    // Update the user with the location
-    await User.findByIdAndUpdate(user._id, { location: location._id });
-
-    // Generate tokens
-    const accessToken = generateAccessToken(user._id, location._id, user.role);
-    const expiredAccessToken = jwt.sign(
-      {
-        sub: user._id,
-        location: location._id,
-        role: user.role,
-        iss: process.env.DEV_DOMAIN,
-        exp: Math.floor(Date.now() / 1000) - 15 * 60,
-      },
-      process.env.JWT_SECRET
-    );
-
-    const invalidAccessToken = jwt.sign(
-      {
-        sub: user._id,
-        location: location._id,
-        role: user.role,
-        iss: process.env.DEV_DOMAIN,
-        exp: Math.floor(Date.now() / 1000) + 15 * 60,
-      },
-      "incorrect_secret"
-    );
-
-    // Return all data needed for tests
-    return {
-      user,
-      location,
-      accessToken,
-      expiredAccessToken,
-      invalidAccessToken,
-    };
+    // Create two test users
   } catch (error) {
     console.error("Error seeding database:", error);
     throw error;
