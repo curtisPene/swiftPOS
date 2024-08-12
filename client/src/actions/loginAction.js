90a190dbc717d47aba2292302db4a75fa4bf04cd
Curtis Pene <curtispene92@gmail.com>
2024-08-12 20:50:50 +1200

    chore: es lin fix

--- client/src/actions/loginAction.js
+++ client/src/actions/loginAction.js
@@ -1,4 +1,4 @@
-import { json, redirect } from "react-router-dom";
+import { redirect } from "react-router-dom";
 
 const loginAction = async ({ request }) => {
   const formData = await request.formData();
